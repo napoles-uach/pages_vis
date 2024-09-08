ae047780a64b88eab24854f7015209922569267c
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2024-09-08 16:22:06 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("MNIST Visualización")
+st.write("Esta aplicación tiene diferentes visualizaciones de los datos de MNIST.")
+st.write("Utiliza el menú de la izquierda para navegar entre las diferentes visualizaciones.")
